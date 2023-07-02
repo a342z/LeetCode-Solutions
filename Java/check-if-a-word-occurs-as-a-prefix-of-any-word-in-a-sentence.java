568c831ced6eaf3e07f58da159e8d82ea8c9888e
zaki <abdulrahman.zaki.342@gmail.com>
2023-07-02 14:18:04 +0300

    check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence

---
+++ Java/check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int isPrefixOfWord(String sentence, String searchWord) {
+        var words = sentence.split(" ");
+        for (int i = 0; i < words.length; i++) {
+            if(words[i].length() >= searchWord.length() && words[i].substring(0, searchWord.length()).equals(searchWord))
+                return i+1 ;
+        }
+        return -1;
+    }
+}
