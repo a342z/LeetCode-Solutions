7a60eed20d7633c6d2aa6696e6ba53130b4950a8
zaki <abdulrahman.zaki.342@gmail.com>
2025-10-22 21:30:52 +0300

    equal-row-and-column-pairs

--- Java/equal-row-and-column-pairs.java
+++ Java/equal-row-and-column-pairs.java
@@ -1,27 +1,22 @@
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 class Solution {
     public int equalPairs(int[][] grid) {
-        Map<String, Integer> map1 = new HashMap<>();
-        Map<String, Integer> map2 = new HashMap<>();
+        Map<List<Integer>, Integer> rowMap = new HashMap<>();
+        int n = grid.length;
 
-        int res = 0;
-        for (int i = 0; i < grid.length; i++) {
-            StringBuilder sb1 = new StringBuilder();
-            StringBuilder sb2 = new StringBuilder();
-            for (int j = 0; j < grid[0].length; j++) {
-                sb1.append(grid[i][j] + ".");
-                sb2.append(grid[j][i] + ".");
-
-            }
-            map1.put(sb1.toString(), map1.getOrDefault(sb1.toString(), 0) + 1);
-            map2.put(sb2.toString(), map2.getOrDefault(sb2.toString(), 0) + 1);
+        for (int[] row : grid) {
+            List<Integer> key = new ArrayList<>();
+            for (int val : row) key.add(val);
+            rowMap.put(key, rowMap.getOrDefault(key, 0) + 1);
         }
 
-        for (var m : map1.entrySet()) {
-            res += map1.getOrDefault(m.getKey(), 0) * map2.getOrDefault(m.getKey(), 0);
+        int res = 0;
 
+        for (int j = 0; j < n; j++) {
+            List<Integer> colKey = new ArrayList<>();
+            for (int i = 0; i < n; i++) colKey.add(grid[i][j]);
+            res += rowMap.getOrDefault(colKey, 0);
         }
 
         return res;
